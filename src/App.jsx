0dac67d506baae88f046fb11478fa77b3d0315a0
KelanaIrsyad <irsyadkelana@gmail.com>
2024-04-08 19:27:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import OutletPage from "./outlets/OutletPage";
+import PageBarangList from "./pages/barang/PageBarangList";
+import PageBarangCreate from "./pages/barang/PageBarangCreate";
+import PageBarangDetail from "./pages/barang/PageBarangDetail";
+import { ContextApplication } from "./libs/config/contexts";
+import { useState } from "react";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  return (
+    <>
+      <ContextApplication.Provider
+        value={{ isAuthenticated, setIsAuthenticated }}
+      >
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<OutletPage />}>
+              <Route index={true} element={<PageBarangList />} />
+              <Route path={"new"} element={<PageBarangCreate />} />
+              <Route path={"detail"} element={<PageBarangDetail />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      </ContextApplication.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/barang/PageBarangList.jsx
@@ -0,0 +1,5 @@
+const PageBarangList = () => {
+    return <h1>Ini halaman Barang</h1>
+}
+
+export default PageBarangList
